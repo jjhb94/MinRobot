9c31cf56647caceb6662662b62f4a7c201601ce6
jesse-baker_DFIN <jesse.baker@dfinsolutions.com>
2025-04-08 10:21:57 -0500

    Notes for future state: fixing enum case too for potential use. Need to udpate db

--- MinRobot/Domain/Models/RobotCommand.cs
+++ MinRobot/Domain/Models/RobotCommand.cs
@@ -4,7 +4,8 @@ public class RobotCommand
 {
     public int CommandId { get; set; } // Database is integer.
     public string RobotId { get; set; } = default!;
-    public string CommandType { get; set; } = default!;
+    public string CommandType { get; set; } = default!; // tried using CommandTypeEnum but it was causing issues with serialization.
+    // maybe we can store enums as INT in the db column for more efficient storage and querying.
     public string CommandData { get; set; } = default!;
     public DateTime CreatedAt { get; set; }
     // public DateTime? ExecutedAt { get; set; }
